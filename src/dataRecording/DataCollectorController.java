e85face422f1d711f0cf2bda6dffe8ef188f2a6d
SergioRM-Patatita <sergio-rosello@hotmail.com>
2016-11-10 12:43:51 +0100

    Updated DataCollectorController so that it can handle imput from any controller.

--- src/dataRecording/DataCollectorController.java
+++ src/dataRecording/DataCollectorController.java
@@ -6,12 +6,35 @@ import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 import pacman.game.Constants.MOVE;
 
+
+
+
+public class DataCollectorController extends Controller<MOVE>{
+
+	private Controller<MOVE> controller;
+
+	public DataCollectorController(Controller<MOVE> input){
+		this.controller = input;
+	}
+
+	@Override
+	public MOVE getMove(Game game, long dueTime) {
+		MOVE move = this.controller.getMove(game, dueTime);
+
+		DataTuple data = new DataTuple(game, move);
+
+		DataSaverLoader.SavePacManData(data);
+		return move;
+	}
+
+}
+
 /**
  * The DataCollectorHumanController class is used to collect training data from playing PacMan.
  * Data about game state and what MOVE chosen is saved every time getMove is called.
  * @author andershh
  *
- */
+
 public class DataCollectorController extends HumanController{
 	
 	public DataCollectorController(KeyBoardInput input){
@@ -29,3 +52,4 @@ public class DataCollectorController extends HumanController{
 	}
 
 }
+*/
